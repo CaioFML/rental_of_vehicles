d1d855ce1909e16ec148c1856e8109826549bf32
CaioFML <caio.felipemotta@hotmail.com>
2021-08-25 19:27:15 -0300

    Ignore some hosts on VCR

--- spec/support/vcr/vcr.rb
+++ spec/support/vcr/vcr.rb
@@ -3,5 +3,7 @@ VCR.configure do |config|
   config.hook_into :webmock
   config.ignore_localhost = true
   config.ignore_host "chromedriver.storage.googleapis.com"
+  config.ignore_host "172.18.0.4"
+  config.ignore_host "chrome"
   config.configure_rspec_metadata!
 end
